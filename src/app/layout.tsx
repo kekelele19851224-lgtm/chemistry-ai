76f6601b2a71d9c58cdcacdd3efb8e9eda747418
kekelele19851224-lgtm <kekelele19851224@gmail.com>
2025-08-15 16:09:52 +0800

    Add Google Analytics tracking
    - Integrate Google Analytics (G-TMTX5Y6B3D) into layout.tsx
    - Add gtag configuration for visitor tracking
    - Maintain compatibility with Next.js static export
    - Update wrangler.toml configuration for Pages deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -164,6 +164,19 @@ export default function RootLayout({
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-TMTX5Y6B3D"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-TMTX5Y6B3D');
+            `,
+          }}
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
